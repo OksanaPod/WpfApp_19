9c7378c0507ae8a1f60133f83e357a09f7f1b5b6
OksanaP <77opod@gmail.com>
2022-10-07 15:49:02 +0300

    Добавьте файлы проекта.

---
+++ WpfApp_19/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_19.Models
+{
+    class Ariph
+    {
+        public static double Calc(int r) => (2 * Math.PI * r);
+    }
+}
